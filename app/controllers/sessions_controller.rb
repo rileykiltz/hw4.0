ab2a6d17126b01de5aba283c035540a8e30197d1
Riley Kiltz <Riley.kiltz@kellogg.northwestern.edu>
2024-06-05 02:34:21 +0000

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params["email"])
+
+    if @user
+      if @user.authenticate(params["password"])
+        # login the user
+        session[:user_id] = @user.id
+        redirect_to "/companies"
+      else
+        # Incorrect password
+        redirect_to "/sessions/new", alert: "Invalid email or password"
+      end
+    else
+      # Email not found
+      redirect_to "/sessions/new", alert: "Invalid email or password"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out successfully"
   end
 end
-  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
     redirect_to "/"
+
+  
   end
 end
